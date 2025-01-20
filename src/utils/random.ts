d28756a5375e69b2ea57068808ef18fe079bd028
chanchal16 <rajputchanchal17@gmail.com>
2025-01-20 14:16:03 +0530

    update type

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,6 +1,7 @@
 import words from "../data/word-lists.json";
+import { wordType } from "../types/types";
 
-export function getRandomWord(type: "adjectives" | "nouns"): string {
+export function getRandomWord(type: wordType): string {
   const list = words[type];
   return list[Math.floor(Math.random() * list.length)];
 }
