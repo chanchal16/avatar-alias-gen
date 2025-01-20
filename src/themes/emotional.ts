aae3b830ec1182732a5514613cf15e5cad525b7d
chanchal16 <rajputchanchal17@gmail.com>
2025-01-20 14:14:31 +0530

    feat: add emotional username generator function

---
+++ src/themes/emotional.ts
@@ -0,0 +1,8 @@
+import { getRandomWord } from "../utils/random";
+
+export const generateEmotionalUsername = (): string => {
+  const emotion = getRandomWord("emotions");
+  const verb = getRandomWord("verbs");
+  const element = getRandomWord("elements");
+  return `${emotion}${verb}${element}`.toLowerCase();
+};
