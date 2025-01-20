12f119183cfb22dc4e579ac4695f2735e3a49ab7
chanchal16 <rajputchanchal17@gmail.com>
2025-01-20 14:14:48 +0530

    feat: add tech username generator function

---
+++ src/themes/tech.ts
@@ -0,0 +1,8 @@
+import { getRandomWord } from "../utils/random";
+
+export const generateTechUsername = (): string => {
+  const techTerm = getRandomWord("tech");
+  const element = getRandomWord("elements");
+  const number = Math.floor(Math.random() * 999);
+  return `${techTerm}${element}${number}`.toLowerCase();
+};
