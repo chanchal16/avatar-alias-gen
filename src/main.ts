59b534597c270c2579df1ae404bff930484f2126
chanchal16 <rajputchanchal17@gmail.com>
2025-01-28 10:11:11 +0530

    update: export generate username

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,15 @@
-import { generateClassicUsername } from "./themes/classic";
-import { generateEmotionalUsername } from "./themes/emotional";
-import { generateElementalUsername } from "./themes/elemental";
-import { generateTechUsername } from "./themes/tech";
 import { Options } from "./types/types";
 import { generateAvatar } from "./utils/avatar";
+import { generateUsername } from "./utils/username";
 
 export async function generateUsernameAndAvatar({
   theme,
   separator = false,
 }: Options): Promise<{ username: string; avatar: string }> {
-  let username = "";
-  switch (theme) {
-    case "classic":
-      username = generateClassicUsername();
-      break;
-    case "emotional":
-      username = generateEmotionalUsername();
-      break;
-    case "elemental":
-      username = generateElementalUsername();
-      break;
-    case "tech":
-      username = generateTechUsername();
-      break;
-    default:
-      username = generateClassicUsername();
-  }
-  if (separator) {
-    username = username.replace(/([a-z])([A-Z])/g, "$1_$2").toLowerCase();
-  }
-
-  username = username.toLowerCase();
-
+  const username = await generateUsername(theme, separator);
   // Generate avatar based on the theme and username
   const avatar = await generateAvatar(theme, username);
   return { username, avatar };
 }
 export { generateAvatar };
+export { generateUsername };
