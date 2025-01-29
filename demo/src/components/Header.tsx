926429b102549108e6ec1c330bca1905b34e355c
chanchal16 <rajputchanchal17@gmail.com>
2025-01-29 18:31:14 +0530

    feat: create header component

---
+++ demo/src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">RandomGen</div>
+      <nav className="nav">
+        <a href="/">Home</a>
+        <a href="/docs">Docs</a>
+        <a href="/github">GitHub</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
